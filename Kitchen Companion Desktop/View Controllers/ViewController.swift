82086924d8149692df49a5ecd3f62a40820afb39
Arnav Pondicherry <arnavpon@arnavs-macbook-pro.local>
2018-07-15 22:10:56 -0400

    initial commit

---
+++ Kitchen Companion Desktop/View Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//  ViewController.swift
+//  Kitchen Companion Desktop
+//  Created by Arnav Pondicherry  on 7/15/18.
+
+// Home Screen for application
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Kitchen Companion DesktopTests/Kitchen_Companion_DesktopTests.swift
@@ -0,0 +1,35 @@
+//
+//  Kitchen_Companion_DesktopTests.swift
+//  Kitchen Companion DesktopTests
+//
+//  Created by Arnav Pondicherry  on 7/15/18.
+//
+
+import XCTest
+@testable import Kitchen_Companion_Desktop
+
+class Kitchen_Companion_DesktopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
